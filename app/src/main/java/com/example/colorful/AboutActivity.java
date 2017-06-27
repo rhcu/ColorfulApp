645841c55e50c283806841cc94c927e251dbf25d
Assiya Kh <assiyakhuzyakhmetova@gmail.com>
2017-06-27 15:32:59 +0600

    added new AboutActivity

---
+++ app/src/main/java/com/example/colorful/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.example.colorful;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+/**
+ * Created by ячсчяс on 27.06.2017.
+ */
+
+public class AboutActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/example/colorful/MainActivity.java
+++ app/src/main/java/com/example/colorful/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.colorful;
 
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -53,5 +54,9 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onVioletButtonClick(View view){
         mAbsoluteLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.violet));
+    }
+    public void infoClick(View view){
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
     }
 }
